669f0257d173e9c2c7c6a6529e8024e11572b012
Cezar <grigorecezar@yahoo.com>
2017-12-28 01:12:21 +0200

    url heroku

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ W1Temp.getSensor('28-0316a1a16fff').then(function (sensor) {
    
    var options = {
      //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
+     url: 'http://pi-temp-api.herokuapp.com/temperature'
      method: 'POST',
      headers: {
        'Content-Type': 'application/json'
