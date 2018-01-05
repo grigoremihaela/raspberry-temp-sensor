495e02f5e0e5a607634e4f2c2a8feb4a61c4b6c4
Cezar <grigorecezar@yahoo.com>
2018-01-05 21:53:58 +0200

    add 2 sensors

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var W1Temp = require('w1temp');
 
 // turn on gpio pin 13 as W1 power if you want to
-W1Temp.setGpioPower(13);
+//W1Temp.setGpioPower(13);
 
 // set gpio pin 6 to use as W1 data channel
 // if is not set by instructions above (required root permissions)
-W1Temp.setGpioData(17);
+//W1Temp.setGpioData(6);
 
 // print list of available sensors uids (ex.: [ '28-00000636a3e3' ])
 W1Temp.getSensorsUids().then(function (sensorsUids) {
   console.log(sensorsUids);
 });
 
-// get instance of temperature sensor// 28-03177067f9ff  // 28-0316a1a16fff
+// get instance of temperature sensor // 28-0316a1a16fff
 W1Temp.getSensor('28-03177067f9ff').then(function (sensor) {
 
   // print actual temperature
@@ -21,30 +21,7 @@ W1Temp.getSensor('28-03177067f9ff').then(function (sensor) {
 
   // print actual temperature on changed
   sensor.on('change', function (temp) {
-    console.log('Temp changed 17:', temp, '°C');
-  });
-
-});
-
-// set gpio pin 6 to use as W1 data channel
-// if is not set by instructions above (required root permissions)
-W1Temp.setGpioData(4);
-
-// print list of available sensors uids (ex.: [ '28-00000636a3e3' ])
-W1Temp.getSensorsUids().then(function (sensorsUids) {
-  console.log(sensorsUids);
-});
-
-// get instance of temperature sensor// 28-03177067f9ff  // 28-0316a1a16fff
-W1Temp.getSensor('28-03177067f9ff').then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp changed 4:', temp, '°C');
+    console.log('Temp changed:', temp, '°C');
   });
 
 });
