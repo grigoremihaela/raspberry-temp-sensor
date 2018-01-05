b4698897fc811db5103f6de49d419891b8f9666b
Cezar <grigorecezar@yahoo.com>
2018-01-05 21:49:07 +0200

    add 2 sensors

--- index.js
+++ index.js
@@ -21,7 +21,31 @@ W1Temp.getSensor('28-03177067f9ff').then(function (sensor) {
 
   // print actual temperature on changed
   sensor.on('change', function (temp) {
-    console.log('Temp changed:', temp, '°C');
+    console.log('Temp changed 17:', temp, '°C');
   });
 
 });
+
+// set gpio pin 6 to use as W1 data channel
+// if is not set by instructions above (required root permissions)
+W1Temp.setGpioData(4);
+
+// print list of available sensors uids (ex.: [ '28-00000636a3e3' ])
+W1Temp.getSensorsUids().then(function (sensorsUids) {
+  console.log(sensorsUids);
+});
+
+// get instance of temperature sensor// 28-03177067f9ff  // 28-0316a1a16fff
+W1Temp.getSensor('28-03177067f9ff').then(function (sensor) {
+
+  // print actual temperature
+  var temp = sensor.getTemperature();
+  console.log('Actual temp:', temp, '°C');
+
+  // print actual temperature on changed
+  sensor.on('change', function (temp) {
+    console.log('Temp changed 4:', temp, '°C');
+  });
+
+});
+
