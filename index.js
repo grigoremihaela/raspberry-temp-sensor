46ea4daad3b70ce44d2505b6d297f04b595c04e3
Cezar <grigorecezar@yahoo.com>
2018-01-05 18:52:06 +0200

    add 2 sensors

--- index.js
+++ index.js
@@ -13,21 +13,20 @@ W1Temp.setGpioData(6);
 W1Temp.getSensorsUids().then(function (sensorsUids) {
   console.log(sensorsUids);
 
-  for (var i = 0; i < sensorsUids.length; i++) {
-     // get instance of temperature sensor
-W1Temp.getSensor(sensorsUids[i]).then(function (sensor) {
+// get instance of temperature sensor
+W1Temp.getSensor(sensorsUids[0]).then(function (sensor) {
 
   // print actual temperature
-  var temp[i] = sensor.getTemperature();
+  var temp = sensor.getTemperature();
   //console.log('Actual temp:', temp, '°C');
 
   // print actual temperature on changed
-  sensor.on('change', function (temp[i]) {
-    console.log('Temp changed '+i+': ', temp[i], '°C');
+  sensor.on('change', function (temp) {
+    console.log('Temp changed 0:', temp, '°C');
 
    // post api send temp
    var json = {
-     "temp": temp[i]
+     "temp": temp
    };
    
    var options = {
@@ -43,7 +42,7 @@ W1Temp.getSensor(sensorsUids[i]).then(function (sensor) {
    request(options, function(err, res, body) {
     console.log(err);
      if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
+       console.log(res.statusCode);
        //console.log(body);
      }
    });
@@ -51,6 +50,43 @@ W1Temp.getSensor(sensorsUids[i]).then(function (sensor) {
    });
 
 });
-} //end for
+
+// get instance of temperature sensor
+W1Temp.getSensor(sensorsUids[1]).then(function (sensor) {
+
+  // print actual temperature
+  var temp = sensor.getTemperature();
+  //console.log('Actual temp:', temp, '°C');
+
+  // print actual temperature on changed
+  sensor.on('change', function (temp) {
+    console.log('Temp changed 1:', temp, '°C');
+
+   // post api send temp
+   var json = {
+     "temp": temp
+   };
+   
+   var options = {
+     //url: 'http://192.168.1.7:3001/temperature',
+     url: 'http://pi-temp-api.herokuapp.com/temperature',
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     json: json
+   };
+
+   request(options, function(err, res, body) {
+    console.log(err);
+     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+       console.log(res.statusCode);
+       //console.log(body);
+     }
+   });
+   // post api send temp
+   });
+
+});
 
 });
