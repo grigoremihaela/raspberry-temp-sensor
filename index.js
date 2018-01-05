9219388e50520f6befec62b5af4005cebba29014
Cezar <grigorecezar@yahoo.com>
2018-01-05 21:44:25 +0200

    add 2 sensors

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var request=require('request');
-
 var W1Temp = require('w1temp');
 
 // turn on gpio pin 13 as W1 power if you want to
@@ -7,86 +5,23 @@ W1Temp.setGpioPower(13);
 
 // set gpio pin 6 to use as W1 data channel
 // if is not set by instructions above (required root permissions)
-W1Temp.setGpioData(6);
+W1Temp.setGpioData(17);
 
 // print list of available sensors uids (ex.: [ '28-00000636a3e3' ])
 W1Temp.getSensorsUids().then(function (sensorsUids) {
   console.log(sensorsUids);
+});
 
-// get instance of temperature sensor
-W1Temp.getSensor(sensorsUids[0]).then(function (sensor) {
+// get instance of temperature sensor// 28-03177067f9ff  // 28-0316a1a16fff
+W1Temp.getSensor('28-0316a1a16fff').then(function (sensor) {
 
   // print actual temperature
   var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
+  console.log('Actual temp:', temp, '°C');
 
   // print actual temperature on changed
   sensor.on('change', function (temp) {
-    console.log('Temp changed 0:', temp, '°C');
-
-   // post api send temp
-   var json = {
-     "temp": temp
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-
-});
-
-// get instance of temperature sensor
-W1Temp.getSensor(sensorsUids[1]).then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp changed 1:', temp, '°C');
-
-   // post api send temp
-   var json = {
-     "temp": temp
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-
-});
+    console.log('Temp changed:', temp, '°C');
+  });
 
 });
