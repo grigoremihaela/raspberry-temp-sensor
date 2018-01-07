a5e61eec09af74f41926013493eb31ed68132129
Cezar <grigorecezar@yahoo.com>
2018-01-07 13:42:34 +0200

    set pin in cron

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var W1Temp = require('w1temp');
 // if is not set by instructions above (required root permissions)
 //W1Temp.setGpioData(27)
 
-W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids1) {
+W1Temp.getSensorsUids('w1_bus_master2').then(function (sensorsUids1) {
   console.log(sensorsUids1);
 // get instance of temperature sensor1 '28-0316a1a16fff'
 W1Temp.getSensor(sensorsUids1).then(function (sensor) {
@@ -54,7 +54,7 @@ W1Temp.getSensor(sensorsUids1).then(function (sensor) {
 });
 });
 
-W1Temp.getSensorsUids('w1_bus_master2').then(function (sensorsUids2) {
+W1Temp.getSensorsUids('w1_bus_master3').then(function (sensorsUids2) {
   console.log(sensorsUids2);
 // get instance of temperature sensor2  '28-03177067f9ff'
 W1Temp.getSensor(sensorsUids2).then(function (sensor) {
