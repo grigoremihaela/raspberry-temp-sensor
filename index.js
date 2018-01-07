3954594db0e01b9d9de71e84cfec5b730a88282f
Cezar <grigorecezar@yahoo.com>
2018-01-07 11:14:15 +0200

    set PINs array

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ var request=require('request');
 
 var W1Temp = require('w1temp');
 
-// turn on gpio pin 13 as W1 power if you want to
-W1Temp.setGpioPower(4);
-// set gpio pin 6 to use as W1 data channel
+var PIN = [4, 27];
+
+for (var i = 0; i < PIN.length; i++) {
+// turn on gpio pin PIN[i] as W1 power if you want to
+W1Temp.setGpioPower(PIN[i]);
+// set gpio pin PIN[i] to use as W1 data channel
 // if is not set by instructions above (required root permissions)
-W1Temp.setGpioData(4)
-// turn on gpio pin 13 as W1 power if you want to
-W1Temp.setGpioPower(27);
-// set gpio pin 6 to use as W1 data channel
-// if is not set by instructions above (required root permissions)
-W1Temp.setGpioData(27)
+W1Temp.setGpioData(PIN[i])
 
 W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids1) {
   console.log(sensorsUids1);
@@ -52,48 +50,9 @@ W1Temp.getSensor(sensorsUids1).then(function (sensor) {
    // post api send temp
    });
 });
-});
+}); // end getSensorsUids
+} // end for
 
-W1Temp.getSensorsUids('w1_bus_master2').then(function (sensorsUids2) {
-  console.log(sensorsUids2);
-// get instance of temperature sensor2  '28-03177067f9ff'
-W1Temp.getSensor(sensorsUids2).then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp changed 2:', temp, '°C ', sensorsUids2[0]);
-
-   // post api send temp
-   var json = {
-     "temp": temp,
-     "sensorsUids": sensorsUids2[0]
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-});
-});
 /*
 // print list of available sensors uids (ex.: [ '28-00000636a3e3' ])
 W1Temp.getSensorsUids().then(function (sensorsUids) {
