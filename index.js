1bc8c7a4cfe12988b7122fc7e66f33f44cf223b1
Cezar <grigorecezar@yahoo.com>
2018-01-08 19:57:13 +0200

    back array uids for pin 4

--- index.js
+++ index.js
@@ -22,168 +22,53 @@ var w1_bus_master = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w
 // if is not set by instructions above (required root permissions)
 //W1Temp.setGpioData(27)
 
+function ShowResults(value, index, ar) {  
+// get instance of temperature sensor2  '28-03177067f9ff'
+W1Temp.getSensor(value).then(function (sensor) {
+
+  // print actual temperature
+  var temp = sensor.getTemperature();
+  //console.log('Actual temp:', temp, '°C');
+
+  // print actual temperature on changed
+  sensor.on('change', function (temp) {
+    console.log('Temp from pin 4/' + (index + 1) + ': ', temp, '°C ', value);
+
+   // post api send temp
+   var json = {
+     "temp": temp,
+     "sensorsUids": value,
+     "pin": 4,
+     "numberSensor": (index + 1)
+   };
+   
+   var options = {
+     //url: 'http://192.168.1.7:3001/temperature',
+     url: 'http://pi-temp-api.herokuapp.com/temperature',
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     json: json
+   };
+
+   request(options, function(err, res, body) {
+    //console.log(err);
+     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+       //console.log(res.statusCode);
+       //console.log(body);
+     }
+   });
+   // post api send temp
+   });
+  });
+}  // end ShowResults
+
 W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids) {
   console.log(sensorsUids);
-// get instance of temperature sensor2  '28-03177067f9ff'
-W1Temp.getSensor(sensorsUids[0]).then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp from pin 4/1:', temp, '°C ', sensorsUids[0]);
-
-   // post api send temp
-   var json = {
-     "temp": temp,
-     "sensorsUids": sensorsUids[0],
-     "pin": 4,
-     "numberSensor": 1
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-});
-
-// get instance of temperature sensor2  '28-03177067f9ff'
-W1Temp.getSensor(sensorsUids[1]).then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp from pin 4/2:', temp, '°C ', sensorsUids[1]);
-
-   // post api send temp
-   var json = {
-     "temp": temp,
-     "sensorsUids": sensorsUids[1],
-     "pin": 4,
-     "numberSensor": 2
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-});
-
-// get instance of temperature sensor2  '28-03177067f9ff'
-W1Temp.getSensor(sensorsUids[2]).then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp from pin 4/3:', temp, '°C ', sensorsUids[2]);
-
-   // post api send temp
-   var json = {
-     "temp": temp,
-     "sensorsUids": sensorsUids[2],
-     "pin": 4,
-     "numberSensor": 3
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-});
-
-// get instance of temperature sensor2  '28-03177067f9ff'
-W1Temp.getSensor(sensorsUids[3]).then(function (sensor) {
-
-  // print actual temperature
-  var temp = sensor.getTemperature();
-  //console.log('Actual temp:', temp, '°C');
-
-  // print actual temperature on changed
-  sensor.on('change', function (temp) {
-    console.log('Temp from pin 4/4:', temp, '°C ', sensorsUids[3]);
-
-   // post api send temp
-   var json = {
-     "temp": temp,
-     "sensorsUids": sensorsUids[3],
-     "pin": 4,
-     "numberSensor": 4
-   };
-   
-   var options = {
-     //url: 'http://192.168.1.7:3001/temperature',
-     url: 'http://pi-temp-api.herokuapp.com/temperature',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     json: json
-   };
-
-   request(options, function(err, res, body) {
-    //console.log(err);
-     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-       //console.log(res.statusCode);
-       //console.log(body);
-     }
-   });
-   // post api send temp
-   });
-});
-}); // W1Temp.getSensorsUids('w1_bus_master1')
+  //sensorsUids.forEach(ShowResults);
+  sensorsUids..forEach(function(value, index) { this.showResults(value, index) });
+}); // end W1Temp.getSensorsUids('w1_bus_master1')
 
 W1Temp.getSensorsUids('w1_bus_master10').then(function (sensorsUids) {
   console.log(sensorsUids);
