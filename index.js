66f7e048ea8e80cc5d2f3ada46225aed97649554
Cezar <grigorecezar@yahoo.com>
2018-01-09 18:32:16 +0200

    PIN :  w1BusMaster

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 var temp=require('./src/api/temp');
 var request=require('request');
-
 var W1Temp = require('w1temp');
+var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, {'pin': 17, 'busMaster': 'w1_bus_master8'}, {'pin': 22, 'busMaster': 'w1_bus_master11'}];
 
-var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22]
-
-var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_bus_master4',  'w1_bus_master5',  
-                   'w1_bus_master6',  'w1_bus_master7',  'w1_bus_master8',  'w1_bus_master9',  'w1_bus_master10', 
-                   'w1_bus_master11'
-                   ];
-
-//var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, {'pin': 17, 'busMaster': 'w1_bus_master8'}, {'pin': 22, 'busMaster': 'w1_bus_master11'}];
-var pinBus = [];
-i = 0;
 // turn on gpio pin 13 as W1 power if you want to
 //W1Temp.setGpioPower(4);
 // set gpio pin 6 to use as W1 data channel
@@ -24,34 +14,16 @@ i = 0;
 // if is not set by instructions above (required root permissions)
 //W1Temp.setGpioData(27)
 
-PIN.forEach(function(pin, bus) {
-  W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
-    sensorsUids.forEach(function(value, index) {
-      W1Temp.getSensor(value).then(function (sensor) {
-        console.log(pin, '   ', w1BusMaster[bus]);
-        if (i==0) {
-          pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
-          i++;
-        };
-        if (i>0 && pinBus[i-1].pin!=pin) {
-          pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
-          i++;
-        };
-      }); // end W1Temp.getSensor
-      console.log('pinBus: ', pinBus);
-    }); // end sensorsUids.forEach
-  }); // end W1Temp.getSensorsUids
-}); // end PIN.forEach
-
-/*
-PIN.forEach(function(pin, bus) {
-  W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
+pinBus.forEach(function(obj, index) {
+  var pin = obj.pin;
+  var bus = obj.busMaster;
+  W1Temp.getSensorsUids(bus).then(function (sensorsUids) {
     sensorsUids.forEach(function(value, index) {
       temp.SendTempApi(value, index, pin);
     }); // end sensorsUids.forEach
   }); // end W1Temp.getSensorsUids
 }); // end PIN.forEach
-*/
+/*
 W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids) {
   //console.log(sensorsUids);
   sensorsUids.forEach(function(value, index) {
@@ -70,3 +42,4 @@ W1Temp.getSensorsUids('w1_bus_master11').then(function (sensorsUids) {
   // get instance of temperature sensor
   temp.SendTempApi(sensorsUids[0], 0, 22)
 });
+*/
---
+++ src/getPinBus.js
@@ -0,0 +1,31 @@
+var request=require('request');
+var W1Temp = require('w1temp');
+var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22]
+var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_bus_master4',  'w1_bus_master5',  
+                   'w1_bus_master6',  'w1_bus_master7',  'w1_bus_master8',  'w1_bus_master9',  'w1_bus_master10', 
+                   'w1_bus_master11'
+                   ];
+var pinBus = [];
+i = 0; 
+
+module.exports.GetPinBus  = function () { 
+PIN.forEach(function(pin, bus) {
+  W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
+    sensorsUids.forEach(function(value, index) {
+      W1Temp.getSensor(value).then(function (sensor) {
+        console.log(pin, '   ', w1BusMaster[bus]);
+        if (i==0) {
+          pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
+          i++;
+        };
+        if (i>0 && pinBus[i-1].pin!=pin) {
+          pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
+          i++;
+        };
+      }); // end W1Temp.getSensor
+    }); // end sensorsUids.forEach
+  }); // end W1Temp.getSensorsUids
+}); // end PIN.forEach
+
+return pinBus;
+}
