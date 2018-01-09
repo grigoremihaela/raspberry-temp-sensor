5c601a476171920678a30cac0e58cb0e9ef39471
Cezar <grigorecezar@yahoo.com>
2018-01-09 17:26:10 +0200

    PIN :  w1BusMaster

--- index.js
+++ index.js
@@ -31,12 +31,13 @@ PIN.forEach(function(pin, bus) {
       W1Temp.getSensor(value).then(function (sensor) {
         console.log(pin, '   ', w1BusMaster[bus]);
         pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
+        console.log('pinBus: ', pinBus);
         i++;
       }); // end W1Temp.getSensor
     }); // end sensorsUids.forEach
   }); // end W1Temp.getSensorsUids
 }); // end PIN.forEach
-console.log('pinBus: ', pinBus);
+
 /*
 PIN.forEach(function(pin, bus) {
   W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
