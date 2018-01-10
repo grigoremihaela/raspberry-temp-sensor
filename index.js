dc8debb6d68310daec5b36d8f6073fbfeb8bf7f2
Cezar <grigorecezar@yahoo.com>
2018-01-11 01:03:59 +0200

    async await

--- index.js
+++ index.js
@@ -33,16 +33,17 @@ const calculatePinBus = new Promise((res, reject) => {
             i++;
           };
           res(pinBus);
-          //console.log('pinBus 1: ', pinBus); // [ { pin: 4, busMaster: 'w1_bus_master1' },{ pin: 17, busMaster: 'w1_bus_master8' },{ pin: 22, busMaster: 'w1_bus_master11' } ]
+          console.log('pinBus 1: ', pinBus); // [ { pin: 4, busMaster: 'w1_bus_master1' },{ pin: 17, busMaster: 'w1_bus_master8' },{ pin: 22, busMaster: 'w1_bus_master11' } ]
         }); // end W1Temp.getSensor
       }); // end sensorsUids.forEach
     }); // end W1Temp.getSensorsUids
   }); // end PIN.forEach
 });
-//async function doIt() {
-  pinBus = await calculatePinBus();
-  console.log('pinBus: ', pinBus);
-//};
+async function doIt() {
+  pinBus = await calculatePinBus()
+  console.log('pinBus: ', pinBus)
+};
+doIt();
 /*
 var getPinBus=require('./src/getPinBus');
 var pinBus = getPinBus.GetPinBus();
