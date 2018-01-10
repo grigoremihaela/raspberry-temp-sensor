a23e22f38acb0fa83ab33c893681cdad1a260e27
Cezar <grigorecezar@yahoo.com>
2018-01-11 01:16:02 +0200

    async await

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var w1_bus_master = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w
                      'w1_bus_master21', 'w1_bus_master22', 'w1_bus_master23', 'w1_bus_master24', 'w1_bus_master25', 
                      'w1_bus_master26'];
 */
-const calculatePinBus = new Promise((res, reject) => {
+new Promise(function(resolve, reject) {
   PIN.forEach(function(pin, bus) {
     W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
       sensorsUids.forEach(function(value, index) {
@@ -32,18 +32,17 @@ const calculatePinBus = new Promise((res, reject) => {
             pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
             i++;
           };
-          res(pinBus);
+          resolve(pinBus);
           console.log(pinBus); // [ { pin: 4, busMaster: 'w1_bus_master1' },{ pin: 17, busMaster: 'w1_bus_master8' },{ pin: 22, busMaster: 'w1_bus_master11' } ]
         }); // end W1Temp.getSensor
       }); // end sensorsUids.forEach
     }); // end W1Temp.getSensorsUids
   }); // end PIN.forEach
-});
-async function doIt() {
-  pinBus = await calculatePinBus(pinBus);
+})
+.then(function(pinBus) {
   console.log('pinBus: ', pinBus);
-};
-doIt();
+});
+
 /*
 var getPinBus=require('./src/getPinBus');
 var pinBus = getPinBus.GetPinBus();
