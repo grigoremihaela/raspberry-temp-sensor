c13597e629dd6d12a40f316e2cbc392fc23acdb8
Cezar <grigorecezar@yahoo.com>
2018-01-11 10:03:09 +0200

    get PinBus ?

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function delay() {
       PIN.forEach(function(pin, bus) {
     W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
       sensorsUids.forEach(function(value, index) {
-        
         W1Temp.getSensor(value).then(function (sensor) {
           //console.log(pin, '   ', w1BusMaster[bus]);
           if (i==0) {
@@ -40,19 +39,16 @@ function delay() {
           resolve(pinBus);
           //console.log(pinBus); // [ { pin: 4, busMaster: 'w1_bus_master1' },{ pin: 17, busMaster: 'w1_bus_master8' },{ pin: 22, busMaster: 'w1_bus_master11' } ]
         }); // end W1Temp.getSensor
-async function asyncCall() {
-  var pinBus = await delay();
-  // expected output: "resolved"
-  console.log('pinBus2: ', pinBus);  
-}
-    
-    
       }); // end sensorsUids.forEach
     }); // end W1Temp.getSensorsUids
   }); // end PIN.forEach
   });
 }
-
+async function asyncCall() {
+  var pinBus = await delay();
+  // expected output: "resolved"
+  console.log('pinBus: ', pinBus);  
+}
 asyncCall(); 
 
 
