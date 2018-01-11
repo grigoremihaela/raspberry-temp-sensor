c86edee00b93e20ad39ef1f136dc456ae6629d8e
Cezar <grigorecezar@yahoo.com>
2018-01-11 17:47:58 +0200

    get SensorsUidsArray

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var request=require('request');
 var W1Temp = require('w1temp');
 var temp=require('./src/api/temp'); 
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
-              {'pin': 17, 'busMaster': 'w1_bus_master8'}, 
-              {'pin': 22, 'busMaster': 'w1_bus_master11'}
+              {'pin': 5, 'busMaster': 'w1_bus_master2'}, 
+              {'pin': 6, 'busMaster': 'w1_bus_master3'},
+              {'pin': 7, 'busMaster': 'w1_bus_master4'}, 
+              {'pin': 8, 'busMaster': 'w1_bus_master5'}, 
+              {'pin': 9, 'busMaster': 'w1_bus_master6'}, 
+              {'pin': 10, 'busMaster': 'w1_bus_master7'},
+              {'pin': 22, 'busMaster': 'w1_bus_master8'}                      
               ];
 pinBus.forEach(function(pinBusMaster) {
   W1Temp.getSensorsUids(pinBusMaster.busMaster).then(function (sensorsUids) {
