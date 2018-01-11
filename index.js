f4e31916c9978d69a96b7445119a5a30e557d248
Cezar <grigorecezar@yahoo.com>
2018-01-11 02:34:10 +0200

    get PinBus ?

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ var w1_bus_master = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w
                      'w1_bus_master26'];
 */
 
+/*
 var getPinBus=require('./src/getPinBus');
 var pinBus = getPinBus.GetPinBus();
 console.log('pinBus: ', pinBus);  // []
+*/
 
 // turn on gpio pin 13 as W1 power if you want to
 //W1Temp.setGpioPower(13);
 // set gpio pin 6 to use as W1 data channel
 // if is not set by instructions above (required root permissions)
 //W1Temp.setGpioData(6)
-/*
+
 // v3  too slowly?
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
               {'pin': 17, 'busMaster': 'w1_bus_master8'}, 
@@ -37,7 +39,7 @@ pinBus.forEach(function(pinBusMaster) {
     }); // end sensorsUids.forEach
   }); // end W1Temp.getSensorsUids
 }); // end PIN.forEach
-*/
+
 /*
 //v1
 W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids) {
