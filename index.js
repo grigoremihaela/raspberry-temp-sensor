25d146745e0164665c90b534272bb16fdd238be8
Cezar <grigorecezar@yahoo.com>
2018-01-11 10:11:16 +0200

    get PinBus ?

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var w1_bus_master = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w
                      'w1_bus_master21', 'w1_bus_master22', 'w1_bus_master23', 'w1_bus_master24', 'w1_bus_master25', 
                      'w1_bus_master26'];
 */
-var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22]
+var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22];
 var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_bus_master4',  'w1_bus_master5',  
                    'w1_bus_master6',  'w1_bus_master7',  'w1_bus_master8',  'w1_bus_master9',  'w1_bus_master10', 
                    'w1_bus_master11'
@@ -50,8 +50,6 @@ async function asyncCall() {
   console.log('pinBus: ', pinBus);  
 }
 asyncCall(); 
-
-
 /*
 var getPinBus=require('./src/getPinBus');
 var pinBus = getPinBus.GetPinBus();
@@ -63,7 +61,7 @@ console.log('pinBus: ', pinBus);  // []
 // set gpio pin 6 to use as W1 data channel
 // if is not set by instructions above (required root permissions)
 //W1Temp.setGpioData(6)
-/*
+
 // v3  too slowly?
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
               {'pin': 17, 'busMaster': 'w1_bus_master8'}, 
@@ -78,7 +76,7 @@ pinBus.forEach(function(pinBusMaster) {
     }); // end sensorsUids.forEach
   }); // end W1Temp.getSensorsUids
 }); // end PIN.forEach
-*/
+
 /*
 //v1
 W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids) {
