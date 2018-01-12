b772566e4e6b2667f606d1b74df16ecb80a90ca3
Cezar <grigorecezar@yahoo.com>
2018-01-12 21:42:07 +0200

    testing

--- src/getPinBus.js
+++ src/getPinBus.js
@@ -9,40 +9,6 @@ var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_
 var pinBus = [];
 var i = 0; 
 
-function delay() {
-  return new Promise((res, reject) => {
-PIN.map(function(pin, bus){
-  return W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
-       sensorsUids.map(function(sensorsUid, index){
-         return W1Temp.getSensor(sensorsUid).then(function (sensor) {
-                 if (i==0) {
-                   pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
-                   i++;
-                 };
-                 if (i>0 && pinBus[i-1].pin!=pin) {
-                   pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
-                   i++;
-                 };
-                 return pinBus;
-         })
-       })
-  }); // return new Promise
-}) // PIN map
-})
-}
-
-async function asyncCall() {
-  var result = await delay();
-  // expected output: "resolved"
-  console.log('result: ', result); 
-}
-
-asyncCall();
-module.exports.GetPinBus  = function () { 
-  console.log('pinBus1: ', pinBus);  // []
-    return pinBus;
-  }
-/*
 const calculatePinBus = new Promise((res, reject) => {
   PIN.forEach(function(pin, bus) {
     W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
@@ -70,7 +36,7 @@ module.exports.GetPinBus  = async function () {
     console.log('pinBus 2: ', pinBus);
     return pinBus;
 }
-*/
+
 /*
 module.exports.GetPinBus  = function () { 
 return new Promise((resolve, reject) => {
