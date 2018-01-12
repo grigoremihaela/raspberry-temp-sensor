091d24244fb4ceb3f189b4fcead16fc951fbe4cd
Cezar <grigorecezar@yahoo.com>
2018-01-12 23:46:10 +0200

    test promise

--- index.js
+++ index.js
@@ -2,16 +2,13 @@
 var request=require('request');
 var W1Temp = require('w1temp');
 var temp=require('./src/api/temp'); 
-
+/*  "sudo dtoverlay w1-gpio gpiopin=4 pullup=0"
 var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22];
 var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_bus_master4',  'w1_bus_master5',  
                    'w1_bus_master6',  'w1_bus_master7',  'w1_bus_master8',  'w1_bus_master9',  'w1_bus_master10', 
                    'w1_bus_master11'
                    ];
-var pinBus = [];
-var i = 0; 
-
-/*
+*/
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
               {'pin': 5, 'busMaster': 'w1_bus_master2'}, 
               {'pin': 7, 'busMaster': 'w1_bus_master3'},
@@ -31,8 +28,9 @@ pinBus.forEach(function(pinBusMaster) {
     }); // end sensorsUids.forEach()
   }); // end W1Temp.getSensorsUids()
 }); // end PIN.forEach()
-*/
 
+/*
+//good
 var sensorsUids = [ '28-031770f1c0ff','28-0516a1dd9cff','28-0316a1d3faff','28-0416a165a5ff' ];
 var pinBus = [];
 
@@ -53,5 +51,26 @@ Promise.all(promises).then(function(results) {
 })
 console.log(pinBus);
 
-
-
+// good
+new Promise((resolve, reject) => {
+  PIN.forEach(function(pin, bus) {
+    W1Temp.getSensorsUids(w1BusMaster[bus]).then(function (sensorsUids) {
+      sensorsUids.forEach(function(value, index) {
+        W1Temp.getSensor(value).then(function (sensor) {
+          //console.log(pin, '   ', w1BusMaster[bus]);
+          if (i==0) {
+            pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
+            i++;
+          };
+          if (i>0 && pinBus[i-1].pin!=pin) {
+            pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
+            i++;
+          };
+          resolve(pinBus);
+          console.log(pinBus); // [ { pin: 4, busMaster: 'w1_bus_master1' },{ pin: 17, busMaster: 'w1_bus_master8' },{ pin: 22, busMaster: 'w1_bus_master11' } ]
+        }); // end W1Temp.getSensor
+      }); // end sensorsUids.forEach
+    }); // end W1Temp.getSensorsUids
+  }); // end PIN.forEach
+});
+*/
