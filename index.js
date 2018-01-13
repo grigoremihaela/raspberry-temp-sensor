e89c7e2cde79dd0be55eb49878c6bcc9f32e1f30
Cezar <grigorecezar@yahoo.com>
2018-01-13 16:59:51 +0200

    test promise

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_
                    'w1_bus_master11'
                    ];
 */
-/*
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
               {'pin': 5, 'busMaster': 'w1_bus_master2'}, 
               {'pin': 7, 'busMaster': 'w1_bus_master3'},
@@ -29,46 +28,7 @@ pinBus.forEach(function(pinBusMaster) {
     }); // end sensorsUids.forEach()
   }); // end W1Temp.getSensorsUids()
 }); // end PIN.forEach()
-*/
-var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22];
-var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_bus_master4',  'w1_bus_master5',  
-                   'w1_bus_master6',  'w1_bus_master7',  'w1_bus_master8',  'w1_bus_master9',  'w1_bus_master10', 
-                   'w1_bus_master11'
-                   ];
-var sensorsUids = [ 
-                    [ '28-031770f1c0ff', '28-0516a1dd9cff', '28-0316a1d3faff', '28-0416a165a5ff' ], 
-                    [ '00-c1e000000000', '00-21e000000000', '00-a1e000000000' ],
-                    [], [], [], [], [],
-                    [ '28-0316a1a16fff' ], [], [], [ '28-03177067f9ff' ]
-                    ];
 
-var pinBus = [];
-var sensorsUid = [];
-var i = 0; 
-function delay() {
-  return new Promise(function(resolve,reject) {
-  PIN.forEach(function(pin, bus) {
-    sensorsUids.forEach(function(sensorsUid, index) {
-          if (sensorsUid && sensorsUid !== [] && i==0) {
-            pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
-            i++;
-          }
-          if (sensorsUid && sensorsUid !== [] && i>0 && pinBus[i-1].pin!=pin) {
-            pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
-            i++;
-          }
-      resolve(pinBus);
-      }); // end sensorsUids.forEach
-  }); // end PIN.forEach
-  });
-}
-async function asyncCall() {
-  var result = await delay();
-  // expected output: "resolved"
-}
-
-asyncCall();
-console.log('pinBus: ', pinBus); 
 /*
 //good
 var sensorsUids = [ '28-031770f1c0ff','28-0516a1dd9cff','28-0316a1d3faff','28-0416a165a5ff' ];
