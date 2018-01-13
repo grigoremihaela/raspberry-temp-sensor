1aaf3a2ce3c80c23db1c1b838ab036285b8d0e8c
Cezar <grigorecezar@yahoo.com>
2018-01-13 09:56:46 +0200

    test promise

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ async function asyncArray() {
   // expected output: "resolved"
   //console.log('sensorsUids1: ', sensorsUids);  
 }
-asyncArray(); 
-console.log('sensorsUids2: ', sensorsUids); 
+
+asyncArray().then( function(sensorsUids) {
+    // Do something with result.
+    console.log('sensorsUids2: ', sensorsUids);
+});
 /*
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
               {'pin': 5, 'busMaster': 'w1_bus_master2'}, 
