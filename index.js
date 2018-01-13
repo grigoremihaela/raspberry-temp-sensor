acea5963aba857f7a480c18b17ce29e607078aab
Cezar <grigorecezar@yahoo.com>
2018-01-13 09:35:22 +0200

    test promise

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 var request=require('request');
 var W1Temp = require('w1temp');
 var temp=require('./src/api/temp'); 
-/*  "sudo dtoverlay w1-gpio gpiopin=4 pullup=0"
+// "sudo dtoverlay w1-gpio gpiopin=4 pullup=0"
 var PIN = [4, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22];
 var w1BusMaster = ['w1_bus_master1',  'w1_bus_master2',  'w1_bus_master3',  'w1_bus_master4',  'w1_bus_master5',  
                    'w1_bus_master6',  'w1_bus_master7',  'w1_bus_master8',  'w1_bus_master9',  'w1_bus_master10', 
                    'w1_bus_master11'
                    ];
-*/
+var sensorsUids = [];
+var pinBus = [];
+/*
 var pinBus = [{'pin': 4, 'busMaster': 'w1_bus_master1'}, 
               {'pin': 5, 'busMaster': 'w1_bus_master2'}, 
               {'pin': 7, 'busMaster': 'w1_bus_master3'},
@@ -28,6 +30,23 @@ pinBus.forEach(function(pinBusMaster) {
     }); // end sensorsUids.forEach()
   }); // end W1Temp.getSensorsUids()
 }); // end PIN.forEach()
+*/
+function delay() {
+var promises = sensorsUids.map(function(sensorsUid){
+  return new Promise(function(resolve,reject) {
+    W1Temp.getSensorsUids('w1_bus_master1').then(function (sensorsUids) {
+      return resolve(sensorsUids);
+    })
+  })
+})
+Promise.all(promises).then(function(results) {
+    console.log('results', results)
+})
+console.log('sensorsUids ', sensorsUids);
+return  sensorsUids;
+}
+sensorsUids = delay();
+console.log(sensorsUids);
 
 /*
 //good
