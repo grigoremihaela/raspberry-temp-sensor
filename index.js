5af9f164fcd1bcb0453273373bea4b4b117824fe
Cezar <grigorecezar@yahoo.com>
2018-01-13 16:24:18 +0200

    test promise

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function delay() {
   return new Promise(function(resolve,reject) {
   PIN.forEach(function(pin, bus) {
     sensorsUids.forEach(function(sensorsUid, index) {
-      if(sensorsUid.length > 0){
+      if(sensorsUid && sensorsUid.length > 0){
           if (i==0) {
             pinBus[i] = { 'pin': pin, 'busMaster': w1BusMaster[bus] };
             i++;
